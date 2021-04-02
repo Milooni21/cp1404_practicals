a7c5a99c4755a54d018792b59317b16cdd7524e4
MILOONI YAGNIK <Milooni21>
2021-04-02 20:46:42 +0530

    new changes

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,13 +1,5 @@
 MINIMUM_LENGTH = 4
 
-def version_1():
-    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-    while len(password) < MINIMUM_LENGTH:
-        password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-    print("*" * len(password))
-
-# version_1()
-
 def main():
     """Get and print password using functions"""
     password = get_password(MINIMUM_LENGTH)
