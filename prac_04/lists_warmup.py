be1042c8a08a00be79469f147f678d071dac000a
MILOONI YAGNIK <Milooni21>
2021-04-08 18:44:52 +0530

    practical 4

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,21 @@
+def main():
+    numbers =[3, 1, 4, 1, 5, 9, 2]
+
+    """
+    1. Change the first element of numbers to "ten" (the string, not the number 10)
+    numbers[0] = "ten"
+    print(numbers)
+    
+    2. Change the last element of numbers to 1
+    numbers[-1] = 1
+    print(numbers)
+    
+    3. Get all the elements from numbers except the first two (slice)
+    print(numbers[2:])
+    
+    4. Check if 9 is an element of numbers
+    print(9 in numbers)
+    
+    """
+
+main()
